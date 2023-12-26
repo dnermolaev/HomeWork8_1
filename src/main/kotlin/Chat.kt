8e53bca357a3771b4a450b3485fd6b45c883666e
Dmitry <boetz@yandex.ru>
2023-12-26 22:02:02 +0700

    Change in Chat.kt

--- src/main/kotlin/Chat.kt
+++ src/main/kotlin/Chat.kt
@@ -1,10 +1,8 @@
-
 data class Chat(
     var chatId: Int = 0,
     var read: Boolean = false,
-    var message: Message,
     var companionId: Int = 0,
-    ) {
+) {
 }
 
 data class Message(
@@ -27,16 +25,15 @@ object chatService {
     var chatsId: Int = 0
 
 
-    fun createMessage(msg: Message, companionId: Int): Int {
+    fun createMessage(msg: Message, userId: Int): Int {
 
         for (chat in chats) {
-            if (chat.companionId == companionId) {
+            if (chat.companionId == userId) {
                 messages.add(msg.copy(msgId = ++messagesId, parentChatId = chat.chatId))
                 return messages.last().msgId
-            }
-             else {
-                var cId  = createChat(companionId, Chat (message = msg))
-                messages.add(msg.copy(msgId = ++messagesId, parentChatId =  cId))
+            } else {
+                var cId = createChat(Chat(companionId = userId))
+                messages.add(msg.copy(msgId = ++messagesId, parentChatId = cId))
             }
         }
         return messages.last().msgId
@@ -62,8 +59,8 @@ object chatService {
         throw MessageNotFoundException("no message with such ID $messageId")
     }
 
-    private fun createChat(userId: Int, chat: Chat): Int {
-        chats.add(chat.copy(chatId = ++chatsId))
+    private fun createChat(chat: Chat): Int {
+        chats.add(chat.copy(companionId = chat.companionId, chatId = ++chatsId))
         return chats.last().chatId
     }
 
@@ -85,58 +82,49 @@ object chatService {
     fun getUnreadChatsCount(chat: Chat): Int {
         val predicate = fun(chat: Chat): Boolean {
             for (message in messages) {
-                if (!chat.message.read) chat.read = false
-                break
+                if (!message.read && message.parentChatId == chat.chatId) {
+                    return false
+                }
             }
-            return chat.read
+            return true
         }
         return chats.filter(predicate).size
     }
 
     @JvmName("getChats1")
-    fun getChats(): List <Chat> {
-            return chats
+    fun getChats(): List<Chat> {
+        return chats
     }
 
-    fun getLastMessages(chatId: Int): List <Message> {
-        val predicate = fun(msg: Message): Boolean {
-            return (messages.size - msg.msgId < 4)
-        }
+    fun getLastMessages(chatId: Int): List<Message> {
+        var messagesWithId = mutableListOf<Message>()
 
+        val predicate = fun(msg: Message): Boolean {
+            if (msg.parentChatId == chatId) {
+                messagesWithId.add(msg.copy())
+            }
+            return (messagesWithId.size - msg.msgId < 4)
+        }
         for (chat in chats) {
             if (chat.chatId == chatId) {
                 return messages.filter(predicate)
             }
 
         }
-        throw ChatNotFoundException("no note with such ID $chatId")
+        throw ChatNotFoundException("no chat with such ID $chatId")
     }
 
-    fun getMessagesList(chatId: Int, cmpnID: Int): Int {
+    fun getMessagesList(chatId: Int, cmpnID: Int): List<Message> {
+        val chat = chats.find { it.chatId == chatId }
+            ?: throw ChatNotFoundException("Чат не найден")
 
-        var messagesToDisplay = mutableListOf<Message>()
-
-        chats
-            .find { it.chatId == chatId }
-            .let { chat ->
-                chat ?: throw ChatNotFoundException("Чат не найден")
-                return@let messages
-            }
+        val messagesToDisplay = messages
             .filter { it.parentChatId == chatId }
             .takeLast(cmpnID)
+            .ifEmpty { throw MessageNotFoundException("no message with such companionID $cmpnID") }
             .onEach { it.read = true }
 
-        for (chat in chats) {
-            if (chat.chatId == chatId) {
-                for (message in messages) {
-                    if (message.companionId == cmpnID) {
-                        message.read = true
-                        messagesToDisplay.add(message)
-                    }
-                }
-                return 1
-            }
-        }
-        throw MessageNotFoundException("no message with such companionID $cmpnID")
+        return messagesToDisplay
     }
 }
+
